653685e494b1a345a41ce3022a0febdc2e7a0bc4
k <you@example.com>
2023-03-18 20:14:03 +0200

    work with Ania in friday

--- e2e-ui-tests/pages/registrationPage.js
+++ e2e-ui-tests/pages/registrationPage.js
@@ -3,20 +3,21 @@ exports.RegistrationPage = class RegistrationPage extends BasePage {
 
     constructor(page) {
         super(page, '/sign-up');
-        this.fullName = page.locator("[placeholder='Full Name']");
+        this.fullName = page.getByPlaceholder('Full name');
         this.email = page.locator("[placeholder='Email address']");
         this.password = page.locator("[type='Password']");
-
-        this.password = page.locator("[type='Password']");
         this.registerButton = page.getByRole('button');
-        
+        this.signInButton = page.locator("[href='/sign-in']");
     }     
 
     async registration(userName, userEmail, userPassword) {
-        await this.fullName.locator(userName);
+        await this.fullName.fill(userName);
         await this.email.type(userEmail);
         await this.password.type(userPassword);
         await this.registerButton.click();
     }
-
+    
+    async clicksignInButton(){
+        await this.signInButton.click(); 
+    } 
 }
--- e2e-ui-tests/specs/registration.spec.js
+++ e2e-ui-tests/specs/registration.spec.js
@@ -1,7 +1,7 @@
 const { test, expect, request } = require('@playwright/test');
-//const { LoginPage } = require('../pages/loginPage');
+const { LoginPage } = require('../pages/loginPage');
 const { RegistrationPage } = require('../pages/registrationPage');
-//const { EditProfilePage } = require('../pages/editProfilePage');
+const { EditProfilePage } = require('../pages/editProfilePage');
 const { users } = require('../../test-data/users');
 const { userData } = require('../../test-data/data');
 const config = require("../../playwright.config");
@@ -18,8 +18,17 @@ test.describe('Registration tests', () => {
     });
 
     test('Registration with valid data', async ({ page }) => {
-        await registrationPage.registration(users[env].user.fullName, users[env].user.email, users[env].user.password);
-        await expect(page.locator("[class='logo']")).toBeVisible;
+    //    await registrationPage.registration(users[env].user.fullName, users[env].user.email, users[env].user.password);
+        await registrationPage.registration('full name', 'email@gmail.com', '123qwe!')
+    // await expect(page.locator("[class='logo']")).toBeVisible;
+    //await expect(page.url()).toContain('/sign-in');
+    await expect(page.locator('h2')).toHaveText('Login form');
     });
+
+    test.only('click sign in link', async ({ page }) => {
+        await registrationPage.clicksignInButton();
+        await expect(page.locator('h2')).toHaveText('Login form');
+        });
+
 
 });
